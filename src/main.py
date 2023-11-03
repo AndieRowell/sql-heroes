a004b0f749b99d95cc9881b26b7bc488ce444bdc
Andie <134252065+AndieRowell@users.noreply.github.com>
2023-11-03 13:29:59 +0000

    adding and fixing commits

--- src/main.py
+++ src/main.py
@@ -37,18 +37,30 @@ def look_up_char():
     superhero_name = input("Who would you like to look up? Type Name Here: ")
         query = """
                 SELECT *
-                FROM superheroes
-                WHERE name = {}").format(sql.Literal(superhero_name))
+                FROM heroes
+                WHERE name = (%s)
                 """
-        if result:
+        execute_select(query, (superhero_name))
             print("Supehero Stats:")
             print("Name:", )
             print("Abilities",)
+#should add and if else here ---
 
 #add a function to add a character
 
 
 #add a function to update a character
-def edit
+def edit_hero_name():
+    superhero_input = input('Enter Current Hero Name: ')
+    new_hero_name = input('Please Enter New Name: ')
+    query = """
+            UPDATE heroes
+            SET name = (%s)
+            WHERE name = (%s)
+            """
+    execute_modify(query, (new_hero_name, superhero_input))
+    print(superhero_input, " changed to ", new_hero_name)
+
+edit_hero_name()
 
 #add a function to delete
